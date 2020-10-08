93031b41a36171faa97caa8c89b2b387d24ea0e2
Testing_author <testing_beta@mail.com>
2020-10-08 23:01:24 +0300

    Creating the RecordSoundViewController(). Configuring the UIStackView.

---
+++ SoundGuess_app/RecordSoundViewController.swift
@@ -0,0 +1,43 @@
+//
+//  RecordSoundViewController.swift
+//  SoundGuess_app
+//
+//  Created by DDDD on 08/10/2020.
+//
+
+import UIKit
+
+class RecordSoundViewController: UIViewController {
+
+    var stackView: UIStackView!
+    
+    override func loadView() {
+        view = UIView()
+        
+        view.backgroundColor = UIColor.gray
+        
+        stackView = UIStackView()
+        stackView.spacing = 30
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = UIStackView.Distribution.fillEqually
+        stackView.alignment = .center
+        stackView.axis = .vertical
+        view.addSubview(stackView)
+        
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
